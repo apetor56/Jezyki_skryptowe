e44ccd7662060779088038e6db9ba7e336e84c96
apetor56 <lukten.github@gmail.com>
2024-03-28 19:04:29 +0100

    part 3 - play with computer

--- 1_kolko_krzyzyk/kolko_krzyzyk.sh
+++ 1_kolko_krzyzyk/kolko_krzyzyk.sh
@@ -3,6 +3,9 @@
 board=(1 2 3 4 5 6 7 8 9)
 currentPlayer="X"
 playerChooseTakenPosition=0
+playWithComputer=0
+
+computer="O"
 
 displayBoard() {
     clear
@@ -31,7 +34,7 @@ isProperPositionTaken() {
 
 isPlayerChoosingWrongPosition() {
     if [ $playerChooseTakenPosition -eq 1 ]; then
-        return
+        return 0
     fi
     
     return 1
@@ -79,8 +82,29 @@ makeMove() {
 displayMenu() {
     clear
     echo "=== MENU ==="
-    echo "1. New game"
-    echo "2. Continue game"
+    echo "1. Play multiplyer"
+    echo "2. Play with computer"
+    echo "3. Continue saved game"
+}
+
+makeChoice() {
+    read -p "Choose an option: " choice
+    
+    case $choice in
+    1)
+        playWithComputer=0
+        ;;
+    2)  
+        playWithComputer=1
+        ;;
+    3)
+        loadGame
+        ;;
+    *)
+        echo "Invalid choice"
+        exit 1
+        ;;
+esac
 }
 
 loadGame() {
@@ -95,57 +119,80 @@ saveGame() {
     echo "board=(${board[@]})" > "saved_game.txt"
     echo "currentPlayer=\"$currentPlayer\"" >> "saved_game.txt"
     echo "playerChooseTakenPosition=$playerChooseTakenPosition" >> "saved_game.txt"
+    echo "playWithComputer=$playWithComputer" >> "saved_game.txt"
     echo "Game saved."
 }
 
-displayMenu
-read -p "Choose an option: " choice
+computerChoosePlace() {
+    randomPosition=$((RANDOM % 9 + 1))
+    if [[ "${board[$randomPosition-1]}" =~ [0-9] ]]; then
+        input=$randomPosition
+    else
+        computerChoosePlace
+    fi
+}
 
-case $choice in
-    1)
-        echo
-        ;;
-    2)  
-        loadGame
-        ;;
-    *)
-        echo "Invalid choice"
-        exit 1
-        ;;
-esac
-
-while true; do
-    displayBoard
-
-    echo -e "\n$currentPlayer player turn, choose position 1-9 or or write \"s\" to save game"
-    read input
-
-    if [ "$input" == "s" ]; then
-        saveGame
-        exit 0
+isNonComputerTurn() {
+    if [ $playWithComputer -eq 0 ] || ([ $playWithComputer -eq 1 ] && [ "$currentPlayer" == "X" ]); then
+        return 0
     fi
 
-    if isProperPositionTaken; then
-        makeMove "$input"
+    return 1
+}
 
-        while isPlayerChoosingWrongPosition; do
-            read position
-            makeMove "$input"
-        done
+isGameWithComputer() {
+    if [ $playWithComputer -eq 1 ]; then
+        return 0
+    fi
 
-        if isWinConditionFullfiled; then
-            displayBoard
-            echo -e "\nPlayer $currentPlayer won!"
-            break
-        elif isDrawConditionFullfiled; then
-            displayBoard
-            echo -e "\nDraw!"
-            break
+    return 1
+}
+
+gameLoop() {
+    while true; do
+        displayBoard
+
+        if ! isGameWithComputer || isNonComputerTurn; then
+            echo -e "\n$currentPlayer player turn, choose position 1-9 or or write \"s\" to save game"
+            read input
         fi
 
-        switchPlayer;
-    else
-        echo -e "\nWrong input. Choose number 1-9."
-        sleep 2
-    fi
-done
+        if [ "$input" == "s" ]; then
+            saveGame
+            exit 0
+        fi
+
+        if isGameWithComputer && [ "$currentPlayer" == "$computer" ]; then
+            computerChoosePlace
+        fi
+
+        if isProperPositionTaken; then
+            makeMove "$input"
+
+            while [ isNonComputerTurn && isPlayerChoosingWrongPosition ]; do
+                read position
+                makeMove "$input"
+            done
+
+            if isWinConditionFullfiled; then
+                displayBoard
+                echo -e "\nPlayer $currentPlayer won!"
+                break
+            elif isDrawConditionFullfiled; then
+                displayBoard
+                echo -e "\nDraw!"
+                break
+            fi
+
+            switchPlayer;
+        else
+            echo -e "\nWrong input. Choose number 1-9."
+            sleep 2
+        fi
+    done
+}
+
+
+displayMenu
+makeChoice
+gameLoop
